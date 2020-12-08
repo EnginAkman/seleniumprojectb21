8642f2c7ad74e4a2ee1801fc2bd3071958027356
EnginAkman <usenakman@gmail.com>
2020-12-08 15:40:48 -0500

    navigate

--- src/test/java/com/cybertek/tests/day1_selenium_intro/SeleniumTest.java
+++ src/test/java/com/cybertek/tests/day1_selenium_intro/SeleniumTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //1-set up the web driver manager
         WebDriverManager.chromedriver().setup();
         //System.setProperty ("chrome", "path of driver")
@@ -17,6 +17,20 @@ public class SeleniumTest {
         //3-and use the instance to get the URL we want
         driver.get("https://www.google.com");
 
+
+        //we are using navigate.back method from SELENIUM LIBRARY
+        Thread.sleep(3000); // freeses the code
+
         driver.navigate().back();
+
+        Thread.sleep(3000);
+
+        driver.navigate().forward();
+        Thread.sleep(3000);
+
+        driver.navigate().to("https://www.tesla.com");
+
+
+
     }
 }
